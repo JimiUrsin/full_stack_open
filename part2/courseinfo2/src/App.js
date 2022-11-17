b5b17f62e7ad1f4b5231f51c1555879557019200
JimiUrsin <32343914+JimiUrsin@users.noreply.github.com>
2022-11-17 21:41:32 +0200

    2.3 done

--- part2/courseinfo2/src/App.js
+++ part2/courseinfo2/src/App.js
@@ -7,13 +7,23 @@ const Part = ({ part }) =>
     {part.name} {part.exercises}
   </p>
 
-const Content = ({ parts }) => parts.map((part) => <Part key={part.id} part={part}/>)
+const Content = ({ parts }) => parts.map(
+  (part) => 
+    <Part key={part.id} part={part}/>
+)
 
-const Course = ({course}) =>
-  <>
-    <Header course={course.name}/>
-    <Content parts={course.parts}/>
-  </>
+const Course = ({course}) => {
+  const total =
+    course.parts.reduce ((s, p) => s + p.exercises, 0)
+  
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total sum={total} />
+    </div>
+  )
+}
 
 const App = () => {
   const course = {
