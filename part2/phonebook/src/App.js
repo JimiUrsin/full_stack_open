5f9c5f9daa9b6ad2e48e991d24f7fc7bb1000622
JimiUrsin <32343914+JimiUrsin@users.noreply.github.com>
2022-11-19 16:14:53 +0200

    2.11 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({newFilter, setNewFilter}) => {
   const handleFilterChange = (event) => {setNewFilter(event.target.value)}
@@ -25,18 +25,21 @@ const PersonForm = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newFilter, setNewFilter] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {setNewName(event.target.value)}
   const handleNumberChange = (event) => {setNewNumber(event.target.value)}
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  })
 
   const addPerson = (event) => {
     event.preventDefault()
