75ea8309242c0f4b10a9ddc936517df5047d7db5
JimiUrsin <32343914+JimiUrsin@users.noreply.github.com>
2022-11-19 18:26:49 +0200

    2.14 done

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -29,8 +29,34 @@ function Country({country}) {
         {listLanguages(country.languages)}
       </ul>
       <img src={country.flags.png} />
+      <Weather city={country.capital} />
     </div>
   )
+}
+
+function Weather({city, countryCode}) {
+  const [weather, setWeather] = useState({})
+
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&units=metric&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`)
+      .then(response => {
+        console.log(response.data)
+        setWeather(response.data)
+      })
+  }, [])
+
+  if ('main' in weather) {
+    return (
+      <div>
+        <p>temperature {weather.main.temp} celsius</p>
+        <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}/>
+        <p>wind {weather.wind.speed} m/s</p>
+      </div>
+    )
+  }
+
+  return <div/>
 }
 
 function Countries ({setFilter, filterCountries}) {
